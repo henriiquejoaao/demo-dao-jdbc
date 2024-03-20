bd142a9bcdfff0303b7826ac9b94db4a15cd4c6b
João Henrique <joaohenriquetrindadeguimaraes@gmail.com>
2024-03-20 19:09:05 -0300

    findById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,11 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -30,7 +26,8 @@ public class DB {
         if (conn != null) {
             try {
                 conn.close();
-            } catch (SQLException e) {
+            }
+            catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
         }
